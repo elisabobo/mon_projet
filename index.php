32adb6d7f9e2a1fe176228ccc65786116916e621
Elisa Chhay <chhayelisa@gmail.com>
2025-02-04 17:04:08 +0100

    index.php : début de la page index

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Krochet & Cnit</title>
+</head>
+<body>
+    <header>
+        <h1>MON SITE DE CROCHET</h1>
+        <nav>
+        <ul>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Crochet</a></li>
+            <li><a href="#">Tricot</a></li>
+            <li><a href="#">Projets personnels</a></li>
+            
+        </ul>
+    </nav>
+    <div>
+        <form>
+            <input type="search" id="recherche" name="q" /> 
+            <button>o</button>
+        </form>
+        <button>S'identifier</button>   
+    </div>
+
+    </header>
+    <footer>
+        <p>Author : Elisa Chhay</p>
+
+    </footer>
+</body>
+</html>
