f88bf0ecf35c3c46578e6bfff00fd41240217653
Elisa Chhay <chhayelisa@gmail.com>
2025-02-19 20:34:56 +0100

    index.php et style.css : début menu burger pour la version mobile

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+    <!--POLICE -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
 
@@ -14,24 +16,27 @@
   
     <header> 
         <nav>
-        <h1>Krochet & Cnit</h1>
-        <ul>
-            <div class="nav-item">
-            <li><a href="#">Articles</a></li>
-            <li><a href="#">Crochet</a></li>
-            <li><a href="#">Tricot</a></li>
-            <li><a href="#">Projets personnels</a></li>
-
-        </ul>
+            <!-- NAVBAR -->
+            <div class="nav_toggle" id="nav-toggle">
+                  <i class="ri-menu-line nav_burger"></i>
+                  <i class="ri-close-line nav_close"></i>
+               </div>
+            <h1>Krochet & Cnit</h1>
+            <ul class="nav-item" id="nav-menu">
+                <li><a href="#">Articles</a></li>
+                <li><a href="#">Crochet</a></li>
+                <li><a href="#">Tricot</a></li>
+                <li><a href="#">Projets personnels</a></li>
+            </ul>
             <form class="barre-recherche">
                 <input type="search" id="recherche" placeholder="Recherche...">
                 <button>
-                <span class="fas fa-search"></span>
-            </button></form> 
-            <div class="burger_menu"><span class="fas fa-bars"></span></div>
-    </nav>
-
+                    <span class="fas fa-search"></span>
+                </button>
+            </form> 
+        </nav>
     </header>
+
     <main>
         <section class="home">
             <h2><a href="#">Patrons de crochet</a></h2>
@@ -39,11 +44,11 @@
         </section>
     </main>
    
-
     <footer>
         <p>Author : Elisa Chhay</p>
         <p>php -S localhost:8000</p>
-
     </footer>
+
+    <script src="assets/js/main.js" type="module"></script>
 </body>
 </html>
